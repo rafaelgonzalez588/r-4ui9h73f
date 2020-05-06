84079322ec1e16a083fc2df84aade68b4eae283b
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-05-06 17:01:45 -0500

    Resolución del reto

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {
+    text: '' ,
+  }
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    })
+  };
+    render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.handleChange} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
